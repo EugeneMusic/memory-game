19a62cff3c9d6a443c8d09e7b7a3e1fe4c160d65
EugeneMusic <muzychenko_eugene@mail.ru>
2022-02-22 03:30:23 +0300

    feat: add cards locker

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ const cards = document.querySelectorAll('.memory-card')
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
         return;
     }
-
     secondCard = this;
-    hasFlippedCard = false;
-
     checkForMatch();
 }
 
@@ -30,13 +29,21 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetBoard();
+        lockBoard = false;
     }, 1500);
+}
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
